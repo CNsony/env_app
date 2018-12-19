66456f455a2252f00e3e0628cabb562773bbdfac
sony cai <sony.l.cai@pwc.com>
2018-12-19 01:00:52 -0600

    net server

--- server.js
+++ server.js
@@ -20,24 +20,31 @@ app.post('/api/world', (req, res) => {
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
-const net = require('net');
-const tcp_server = net.createServer();
+var net = require('net');
+var PORT = 3000;
 
-tcp_server.on("connection",function(socket){
-  console.log("CONNECTED: "+ sock.remoteAddress +':'+ sock.remotePort)
-})
-
-tcp_server.on('data', function(data) {
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+    
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
         
-  console.log('DATA ' + sock.remoteAddress + ': ' + data);
-  // Write the data back to the socket, the client will receive it as data from the server
-  sock.write('You said "' + data + '"');
-  
-});
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+        
+    });
+    
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+    
+}).listen(PORT);
 
-// Add a 'close' event handler to this instance of socket
-tcp_server.on('close', function(data) {
-  console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-});
-
-tcp_server.listen(3000,()=>{console.log(`Listening on port 3000: net server`)})
+console.log('Server listening on '+':'+ PORT);
